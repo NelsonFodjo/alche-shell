ea09feb57919b96f7483210dbd14d649fa960d36
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-03-23 12:49:57 -0700

    Added the file 0-iam_betty

---
+++ permissions/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p 'Enter your file name: ' file
+read -p 'Enter the command: ' cmd
+
+touch "$file"
+echo -e "#!/bin/bash\n${cmd}\n" > file
+chmod u+x "$file"
+
+git add .
+git commit -m "Added the file ${file}"
+git push
